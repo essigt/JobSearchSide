16891c17effb84948d1e4653502277f43f8acaa6
essigt <t.essig@essignetz.de>
2013-10-31 12:35:23 +0100

    Initial commit

---
+++ routes/job.js
@@ -0,0 +1,37 @@
+var titles = ["Software Engeneer", "Firefighter", "Software Architect", "Teacher", "Big Boss"];
+var locations = ["Karlsruhe", "Karlsruhe-Mühlburg", "Karlsruhe-Süd", "Karlsruhe-Reinhafen", "Eppingen", "San Francisco", "Sydney"];
+
+/**
+
+*/
+exports.list = function(req, res){
+  res.send("all jobs");
+};
+
+
+function generateJobs(min, max) {
+    // Generate some Random Jobs
+    var generatedList = new Array();    
+    var num = (Math.random()*(max-min))+min; //A number between min and max
+    
+    for(var i=0; i < num; i++) {
+        var titleId = Math.round(Math.random()* (titles.length-1));
+        var locationId = Math.round(Math.random()* (locations.length-1));
+        
+        generatedList.push({title: titles[titleId], location: locations[locationId]});    
+    }
+    
+    return generatedList;
+}
+
+/**
+
+*/
+exports.search = function(req, res) {    
+    var title = req.params.title;
+    var location = req.params.location;
+    
+    
+    //TODO: Actualy search jobs
+    res.send( generateJobs(4,20) );    
+}
